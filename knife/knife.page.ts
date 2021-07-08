a759d87e7b4dd503efd0279d76fea7715c3d7a03
Your Name <you@example.com>
2021-07-08 17:06:46 +0700

    complete2

--- knife/knife.page.ts
+++ knife/knife.page.ts
@@ -6,23 +6,24 @@ import { ApiService } from "../api.service";
 
 class GameScene extends Phaser.Scene {
   //data of knife:
+  valuer: any = {
+    ship: 5,
+  };
   gift: any;
   thunder: any;
-  image: any;
   timer = 0;
   user: any;
   api: any;
-  score: any;
   apple: any;
   knife1: any;
   target: any;
   game: any;
   knifeGroup: any;
   canThrow: boolean;
-  value = 100;
+  value = 1;
   gameOptions: any = {
     // target rotation speed, in degrees per frame
-    rotationSpeed: 4,
+    rotationSpeed: 0.7,
     // knife throwing duration, in milliseconds
     throwSpeed: 150,
     // minimum angle between two knives
@@ -32,7 +33,7 @@ class GameScene extends Phaser.Scene {
     // interval before next rotation speed variation, in milliseconds
     changeTime: 2000,
     // maximum rotation speed, in degrees per frame
-    maxRotationSpeed: 8,
+    // maxRotationSpeed: 8,
     timeLimit: 31,
   };
 
@@ -48,18 +49,15 @@ class GameScene extends Phaser.Scene {
   timeText: Phaser.GameObjects.BitmapText;
   timerEvent: any;
   showTimer = false;
-  giftGroup: Phaser.GameObjects.Group;
   text: Phaser.GameObjects.Text;
   sign1 = Phaser.Math.Between(2, 10);
   gems: Phaser.GameObjects.Sprite;
+  starfield: Phaser.GameObjects.TileSprite;
   constructor(config) {
     super(config);
   }
   preload() {
-    //  for (var i = 1; i < 7; i++) {
-    //    this.load.image(""+i, "images/" + i + ".png");
-    //  }
-    this.load.image("image", "assets/images/1.png");
+    this.load.image("bg", "assets/images/bg_shoot.jpg");
     this.load.image("thunder", "assets/images/thunder.png");
     this.load.image("gift", "assets/images/gift.png");
     this.load.bitmapFont(
@@ -84,15 +82,12 @@ class GameScene extends Phaser.Scene {
     this.api = this.registry.get("api");
   }
   start() {
+    const { width, height } = this.cameras.main;
     // can the player throw a knife? Yes, at the beginning of the game
 
     this.canThrow = true;
-    this.knife1 = this.add.sprite(
-      this.comp.api.screen_size.w / 2,
-      this.comp.api.screen_size.h / 1.2,
-      "knife1"
-    );
-    this.knife1.displayWidth = this.comp.api.screen_size.w / 10;
+    this.knife1 = this.add.sprite(width / 2, height * 0.7, "knife1");
+    this.knife1.displayWidth = this.comp.api.screen_size.w / 6;
     this.knife1.displayHeight = this.knife1.displayWidth * 2;
     if (this.thingsGroup) {
       this.input.on("pointerdown", this.throwKnife, this);
@@ -100,7 +95,7 @@ class GameScene extends Phaser.Scene {
   }
   next() {
     this.timer = 0;
-    this.value = 100;
+    this.value = 1;
     this.text.visible = false;
     this.gems.setVisible(false);
     this.timeText.visible = false;
@@ -116,13 +111,36 @@ class GameScene extends Phaser.Scene {
     this.timeText.visible = false;
   }
   create() {
+    const { width, height } = this.cameras.main;
     // at the beginning of the game, both current rotation speed and new rotation speed are set to default rotation speed
-    this.currentRotationSpeed = this.gameOptions.rotationSpeed;
-    this.newRotationSpeed = this.gameOptions.rotationSpeed;
+    this.currentRotationSpeed = this.gameOptions.rotationSpeed *this.valuer.ship
+    this.newRotationSpeed = this.gameOptions.rotationSpeed * this.valuer.ship;
+    console.log(this.currentRotationSpeed+"tốc độ quay hiện tại");
+    console.log(this.newRotationSpeed +"tốc độ quay mới");
+
+    // scroll-y bg;
+    this.starfield = this.add
+      .tileSprite(0, 0, 0, 0, "bg")
+      .setDisplaySize(this.scale.width, this.scale.height)
+      .setOrigin(0, 0);
+
+    // create range
+    this.registry.events.on(
+      "changedata",
+      (parent, key, value) => {
+        if (key == "set_power") {
+          this.valuer = value;
+          console.log('213213')
+        }
+      },
+      this
+    );
+    // tạo timeText
     this.createTimetext();
     if (this.timeText == null) {
       this.createTimetext();
     }
+    // create text
     this.text = this.add.text(
       this.comp.api.screen_size.w / 10,
       this.comp.api.screen_size.h / 10,
@@ -138,32 +156,22 @@ class GameScene extends Phaser.Scene {
 
     // group to store all rotating knives
     this.knifeGroup = this.add.group();
-    // adding the knife
 
     // adding the target
-    this.target = this.add.sprite(
-      this.comp.api.screen_size.w / 2,
-      this.comp.api.screen_size.h / 5.5,
-      "target"
-    );
-    this.target.displayWidth = this.comp.api.screen_size.w / 3;
+    this.target = this.add.sprite(width / 2, height / 8, "target");
+    this.target.displayWidth = this.comp.api.screen_size.w / 1.8;
     this.target.scaleY = this.target.scaleX;
     this.target.depth = 1;
 
     this.thunder = this.add.sprite(
-      this.comp.api.screen_size.w / 2,
-      this.comp.api.screen_size.h / 4.5,
+      width / 2.2,
+      height /3.5,
       "thunder"
     );
-    this.thunder.displayWidth = this.comp.api.screen_size.w;
-    this.thunder.scaleY = this.thunder.scaleX;
+    this.thunder.displayWidth = this.comp.api.screen_size.w *2;
+    this.thunder.scaleY = this.thunder.scaleX ;
     this.thunder.depth = 3;
     this.thunder.visible = false;
-    // this.image = this.add.sprite(
-    //   this.comp.api.screen_size.w / 2,
-    //   this.comp.api.screen_size.h / 2,
-    //   "image"
-    // );
 
     // moving the target on front
     //this.target.depth = 1;
@@ -171,7 +179,6 @@ class GameScene extends Phaser.Scene {
     // determing apple angle in radians
     var radians = Phaser.Math.DegToRad(90);
     for (let i = 0; i < 6; i++) {
-      // var apple:any = this.add.sprite(this.target.x[i], this.target.y[i], "apple");
       var apple: any = this.add.sprite(
         this.target.x + (this.target.displayWidth / 1.4) * Math.cos(radians),
         this.target.y + (this.target.displayWidth / 1.4) * Math.sin(radians),
@@ -195,6 +202,7 @@ class GameScene extends Phaser.Scene {
         frameRate: 16,
         repeat: -1,
       });
+
       this.gems = this.add
         .sprite(
           this.comp.api.screen_size.w / 15,
@@ -207,10 +215,11 @@ class GameScene extends Phaser.Scene {
     }
     this.text.visible = false;
     this.createG();
+
     // waiting for player input to throw a knife
     // this is how we create a looped timer event
     let timedEvent = this.time.addEvent({
-      delay: Phaser.Math.Between(3000, 6000),
+      delay: 8000,
       callback: this.changeSpeed,
       callbackScope: this,
       loop: true,
@@ -251,20 +260,20 @@ class GameScene extends Phaser.Scene {
       this.gameOptions.rotationVariation
     );
     // new rotation speed
-    this.newRotationSpeed = (this.currentRotationSpeed + variation) * sign;
+    this.newRotationSpeed = Math.floor((this.currentRotationSpeed *this.valuer.ship + variation) * sign);
+    console.log(this.newRotationSpeed+"+++")
 
     // setting new rotation speed limits
     this.newRotationSpeed = Phaser.Math.Clamp(
       this.newRotationSpeed,
-      -this.gameOptions.maxRotationSpeed,
-      this.gameOptions.maxRotationSpeed
+      -this.gameOptions.rotationSpeed * this.valuer.ship,
+      this.gameOptions.rotationSpeed * this.valuer.ship
     );
   }
   tick() {
     this.timeText.visible = true;
     this.text.visible = true;
     this.gems.setVisible(true);
-    console.log(this.timeText);
     this.timer++;
     this.timeText.text = (this.gameOptions.timeLimit - this.timer).toString();
     if (this.timer >= this.gameOptions.timeLimit) {
@@ -287,6 +296,7 @@ class GameScene extends Phaser.Scene {
   }
 
   throwKnife() {
+    const { width, height } = this.cameras.main;
     // can the player throw?
     if (this.canThrow) {
       this.count_throw++;
@@ -331,6 +341,7 @@ class GameScene extends Phaser.Scene {
               var explosion = this.add
                 .sprite(this.knife1.x, this.knife1.y / 0.8, "kaboom")
                 .play("boom");
+              explosion.setScale(0.5);
               explosion.depth = 2;
               explosion.once("animationcomplete", () => {
                 explosion.destroy();
@@ -348,10 +359,8 @@ class GameScene extends Phaser.Scene {
             ok = true;
             // hộp quà huỷ
             this.gift.visible = false;
-
             // đếm lùi
             this.addTimer();
-            // console.log(this.gift);
             console.log("trung hộp quà");
             // hiệu ứng nổ
             var explosion = this.add
@@ -361,21 +370,18 @@ class GameScene extends Phaser.Scene {
             explosion.once("animationcomplete", () => {
               explosion.destroy();
             });
-            this.value = 100 * this.sign1;
+            this.value = this.valuer.ship * this.sign1;
           }
           // is this a legal hit?
           if (this.legalHit && ok) {
             this.canThrow = true;
-            var value = this.value;
-            this.comp.api.user.balance += value;
-            this.comp.api.animate(value);
-            var knife1 = this.add.sprite(
-              this.comp.api.screen_size.w / 2,
-              this.comp.api.screen_size.h * 1.2,
-              "knife1"
-            );
+            var point1 = this.value * this.valuer.ship;
+            console.log(this.valuer)
+            this.comp.api.user.balance += point1;
+            this.comp.api.animate(point1);
+            var knife1 = this.add.sprite(width / 2, height * 0.7, "knife1");
 
-            knife1.displayWidth = this.comp.api.screen_size.w / 10;
+            knife1.displayWidth = this.comp.api.screen_size.w / 6;
             knife1.displayHeight = this.knife1.displayWidth * 2;
 
             knife1.impactAngle = this.target.angle;
@@ -384,22 +390,13 @@ class GameScene extends Phaser.Scene {
               knife1.destroy();
             }, 2000);
 
-            // var knifes = this.knifeGroup.getChildren();
-            // for (let m = 0; m < knifes.length; m++) {
-            //   setTimeout(() => {
-            //     knifes[m].destroy();
-            //   }, 2000);
-
-            //   break;
-            // }
-            // bringing back the knife to its starting position
-            this.knife1.y = this.comp.api.screen_size.h / 1.1;
+            this.knife1.y = height * 0.7;
           }
           // in case this is not a legal hit
           else {
-            var value = this.value;
-            this.comp.api.user.balance -= value;
-            this.comp.api.animate(-value);
+            var point = this.value*this.valuer.ship;
+            this.comp.api.user.balance -= point;
+            this.comp.api.animate(-point);
             this.thunder.visible = true;
             this.cameras.main.flash();
             setTimeout(() => {
@@ -433,13 +430,11 @@ class GameScene extends Phaser.Scene {
     }
   }
   sendData(value) {
-    //send api this.comp.api.post('action=knife', {value: value}).subcribe(r=>{this.count_throw = 0})
     if (this.count_throw == 5) {
       this.comp.api
         .post("user?action=knife", { value: value })
         .subscribe((r) => {
           this.count_throw = 0;
-          console.log(this.comp.api.user.balance);
         });
     }
   }
@@ -452,8 +447,11 @@ class GameScene extends Phaser.Scene {
       this.target.y + (this.target.displayWidth / 1.7) * Math.sin(radians1);
   }
   update(time, delta) {
+    // console.log(this.valuer)
     // rotating the target
     this.target.angle += this.currentRotationSpeed;
+    this.starfield.tilePositionY -= 2;
+
     var things = this.thingsGroup.getChildren();
     for (var i = 0; i < things.length; i++) {
       things[i].angle += this.currentRotationSpeed;
@@ -498,14 +496,16 @@ class GameScene extends Phaser.Scene {
   styleUrls: ["./knife.page.scss"],
 })
 export class KnifePage implements OnInit {
-  phaserGame: Phaser.Game;
   config: any;
-
+  value: any = {
+    ship: 5,
+  };
+  show = 0;
   game: any;
   constructor(public api: ApiService) {
     this.config = {
-      width: this.api.screen_size.w,
-      height: this.api.screen_size.h,
+      width: 500,
+      height: "100%",
       type: Phaser.AUTO,
       mode: Phaser.Scale.FIT,
       autoCenter: Phaser.Scale.CENTER_BOTH,
@@ -517,7 +517,38 @@ export class KnifePage implements OnInit {
     };
   }
   ngOnInit() {
-    this.phaserGame = new Phaser.Game(this.config);
-    this.phaserGame.registry.set("comp", this);
+    
+
+    setTimeout(() => {
+      this.show = 1;
+    }, 1000);
+  }
+  setValue(type, v) {
+    this.value[type] = v;
+    this.game.registry.set("set_power", this.value);
+    console.log(this.value)
+  }
+  ionViewDidEnter() {
+    this.api.sound.volume(0.1);
+
+    this.game = new Phaser.Game(this.config);
+    if (this.game) {
+      this.game.registry.set("comp", this);
+      this.game.registry.set("api", this.api);
+      this.game.registry.set("set_power", this.value);
+
+      setTimeout(() => {
+        this.show = 1;
+      }, 1000);
+    }
+    // this.game.scene.resume('MainScene');
+  }
+
+  ionViewWillLeave() {
+    this.show = 0;
+    //gui du lieu truoc khi thoat
+    this.api.sound.volume(1);
+    // this.game.scene.pause('MainScene');
+    this.game.destroy(true);
   }
 }
